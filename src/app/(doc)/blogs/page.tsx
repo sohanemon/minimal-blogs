1dc1334c93b11ce8c02f9e10841333df66275d62
sohanemon <m.sohanemon@gmail.com>
2023-06-12 22:23:53 +0600

    mdx: added

--- src/app/(doc)/blogs/page.tsx
+++ src/app/(doc)/blogs/page.tsx
@@ -2,11 +2,7 @@ import Lol from './lol.mdx';
 export default function Page() {
   return (
     <>
-      <h1 className='text-red-500 text-7xl'>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Non nostrum eum
-        do
-      </h1>
-      <div className='prose prose-stone'>
+      <div className='prose'>
         <Lol />
       </div>
     </>
