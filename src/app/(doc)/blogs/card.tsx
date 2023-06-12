e2883afa102caedfc11154e9fd9134308bb1dbb9
sohanemon <m.sohanemon@gmail.com>
2023-06-12 16:54:02 +0000

    Jun 12, 2023, 10:54 PM

--- src/app/(doc)/blogs/card.tsx
+++ src/app/(doc)/blogs/card.tsx
@@ -2,6 +2,9 @@ export default function Card() {
   return (
     <div className='@container mx-auto px-2 border max-w-sm border-neutral-400 rounded-lg py-2'>
       <p>How to add MDX in Next JS</p>
+      <p>
+        MDX is a popular library to add Markdown into your React application.
+      </p>
     </div>
   );
 }
