81ca402b0b0a4c192dab341a7a0e6cc005be6706
sohanemon <m.sohanemon@gmail.com>
2023-06-12 16:48:04 +0000

    Jun 12, 2023, 10:48 PM

--- src/app/(doc)/blogs/page.tsx
+++ src/app/(doc)/blogs/page.tsx
@@ -1,7 +1,7 @@
 export default function Page() {
   return (
-    <main className='@container'>
-      <h1 className='my-4 text-3xl text-center capitalize'>
+    <main className='container '>
+      <h1 className='my-4 text-3xl font-semibold text-center capitalize'>
         Recent blogs by sohanEmon
       </h1>
     </main>
