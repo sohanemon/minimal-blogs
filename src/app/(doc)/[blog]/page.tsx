caf57efa602065f60bdb27d8004cd2cdee1f134e
sohanemon <m.sohanemon@gmail.com>
2023-06-12 17:47:30 +0000

    Jun 12, 2023, 11:47 PM

--- src/app/(doc)/[blog]/page.tsx
+++ src/app/(doc)/[blog]/page.tsx
@@ -2,7 +2,7 @@ import Profile from '../blogs/profile';
 import Mdx from './mdx-in-next-js.mdx';
 export default function Page() {
   return (
-    <section className='max-w-3xl p-8 mx-auto prose prose-invert'>
+    <section className='max-w-3xl p-8 mx-auto prose prose-invert bg-neutral-800'>
       <h1>MDX in Next js</h1>
       <Profile mini />
       <Mdx />
