760cd6435f89ce2e416677ef48acdc4688f94d7c
sohanemon <m.sohanemon@gmail.com>
2023-06-12 17:27:39 +0000

    Jun 12, 2023, 11:27 PM

--- src/app/(doc)/blogs/page.tsx
+++ src/app/(doc)/blogs/page.tsx
@@ -4,7 +4,7 @@ import Profile from './profile';
 export default function Page() {
   return (
     <main className='container @container max-w-4xl'>
-      <h1 className='my-4 text-3xl font-semibold text-center capitalize'>
+      <h1 className='my-8 text-3xl font-semibold text-center capitalize'>
         Recent blogs by sohanEmon
       </h1>
       <div className='flex gap-10 mt-10'>
--- src/app/(doc)/blogs/social.tsx
+++ src/app/(doc)/blogs/social.tsx
@@ -1,23 +1,30 @@
 import { Github, Linkedin, Twitter } from 'lucide-react';
+import Link from 'next/link';
 
 export default function Social() {
   return (
     <div className='flex items-center justify-center gap-10 mt-1'>
-      <Github
-        size={20}
-        strokeWidth={1}
-        className='transition-colors hover:fill-neutral-300'
-      />
-      <Twitter
-        size={20}
-        strokeWidth={1}
-        className='transition-colors hover:fill-neutral-300'
-      />
-      <Linkedin
-        size={20}
-        strokeWidth={1}
-        className='transition-colors hover:fill-neutral-300'
-      />
+      <Link href={'https://github.com/sohanemon'}>
+        <Github
+          size={20}
+          strokeWidth={1}
+          className='transition-colors hover:fill-neutral-300'
+        />
+      </Link>
+      <Link href={'https://twitter.com/sohanemon'}>
+        <Twitter
+          size={20}
+          strokeWidth={1}
+          className='transition-colors hover:fill-neutral-300'
+        />
+      </Link>
+      <Link href={'https://linkedin.com/in/sohanemon'}>
+        <Linkedin
+          size={20}
+          strokeWidth={1}
+          className='transition-colors hover:fill-neutral-300'
+        />
+      </Link>
     </div>
   );
 }
