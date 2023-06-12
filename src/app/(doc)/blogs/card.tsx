255a8e2cd31523e57a7e81c86a4b52082515fc85
sohanemon <m.sohanemon@gmail.com>
2023-06-12 16:51:02 +0000

    Jun 12, 2023, 10:51 PM

--- src/app/(doc)/blogs/card.tsx
+++ src/app/(doc)/blogs/card.tsx
@@ -1,3 +1,7 @@
 export default function Card() {
-  return <div></div>;
+  return (
+    <div className='@container container max-w-sm border-neutral-400 rounded-xl'>
+      <p>How to add MDX in Next JS</p>
+    </div>
+  );
 }
--- src/app/(doc)/blogs/page.tsx
+++ src/app/(doc)/blogs/page.tsx
@@ -1,9 +1,12 @@
+import Card from './card';
+
 export default function Page() {
   return (
     <main className='container '>
       <h1 className='my-4 text-3xl font-semibold text-center capitalize'>
         Recent blogs by sohanEmon
       </h1>
+      <Card />
     </main>
   );
 }
