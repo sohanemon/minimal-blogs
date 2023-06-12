da2032362e40efd8624e1ffe08c861f35f280e79
sohanemon <m.sohanemon@gmail.com>
2023-06-12 17:41:20 +0000

    Jun 12, 2023, 11:41 PM

--- src/app/(doc)/blogs/profile.tsx
+++ src/app/(doc)/blogs/profile.tsx
@@ -8,7 +8,7 @@ export default function Profile({ mini }: { mini?: boolean }) {
       className={cn(
         'flex flex-col items-center py-4 border rounded-lg w-80 h-max border-neutral-700',
         {
-          '': mini,
+          'not-prose border-none': mini,
         }
       )}
     >
@@ -17,7 +17,9 @@ export default function Profile({ mini }: { mini?: boolean }) {
         alt='author'
         width={100}
         height={100}
-        className='scale-75 rounded-full'
+        className={cn('scale-75 rounded-full', {
+          'scale-100 w-10 aspect-square': mini,
+        })}
       />
       <p className='font-semibold'>Sohan R. Emon</p>
       <p className='text-xs opacity-70'>Web Developer</p>
