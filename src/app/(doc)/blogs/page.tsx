6eea28c6beaabaf35a829817eb4f8672cd227181
sohanemon <m.sohanemon@gmail.com>
2023-06-12 15:41:12 +0000

    Jun 12, 2023, 9:41 PM

--- src/app/(doc)/blogs/page.tsx
+++ src/app/(doc)/blogs/page.tsx
@@ -1,8 +1,10 @@
+import Lol from './lol.mdx';
 export default function Page() {
   return (
     <h1 className='prose'>
       Lorem ipsum dolor sit amet consectetur adipisicing elit. Non nostrum eum
       do
+      <Lol />
     </h1>
   );
 }
