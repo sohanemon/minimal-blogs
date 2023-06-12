e43674b58a27f77a5cb09af8f4d9ec072b9f7d34
sohanemon <m.sohanemon@gmail.com>
2023-06-12 18:08:09 +0000

    Jun 13, 2023, 12:08 AM

--- src/app/(doc)/[blog]/page.tsx
+++ src/app/(doc)/[blog]/page.tsx
@@ -1,9 +1,14 @@
+import GoBack from '@/components/go-back';
 import Profile from '../blogs/profile';
 import Mdx from './mdx-in-next-js.mdx';
 export default function Page() {
   return (
     <section className='max-w-3xl p-8 mx-auto prose prose-invert bg-neutral-800'>
-      <h1>MDX in Next js</h1>
+      <div className='relative'>
+        <GoBack className='absolute p-2 rounded-full -left-32 text-neutral-600 ring-1 ring-neutral-700' />
+        <h1>MDX in Next js</h1>
+      </div>
+
       <Profile mini />
       <Mdx />
     </section>
--- src/components/go-back.tsx
+++ src/components/go-back.tsx
@@ -1,4 +1,12 @@
 'use client';
-export default function GoBack() {
-  return <></>;
+
+import { MoveLeft } from 'lucide-react';
+import { HTMLAttributes } from 'react';
+
+export default function GoBack(props: HTMLAttributes<HTMLDivElement>) {
+  return (
+    <div {...props}>
+      <MoveLeft />
+    </div>
+  );
 }
