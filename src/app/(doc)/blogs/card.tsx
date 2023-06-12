1051f64a0743186dff7c9175bba8f72be652203e
sohanemon <m.sohanemon@gmail.com>
2023-06-12 17:03:21 +0000

    Jun 12, 2023, 11:03 PM

--- src/app/(doc)/blogs/card.tsx
+++ src/app/(doc)/blogs/card.tsx
@@ -1,10 +1,24 @@
-export default function Card() {
+import { cn } from '@/lib/utils';
+import { HTMLAttributes } from 'react';
+
+export default function Card({ className }: HTMLAttributes<HTMLDivElement>) {
   return (
-    <div className='@container mx-auto px-2 border max-w-sm border-neutral-400 rounded-lg py-2'>
-      <p>How to add MDX in Next JS</p>
-      <p>
-        MDX is a popular library to add Markdown into your React application.
+    <div
+      className={cn(
+        'mx-auto px-5 border border-neutral-700 hover:border-neutral-600 transition-all group rounded-lg py-7 [&>*]:transition-all',
+        className
+      )}
+    >
+      <p className='font-semibold opacity-95 group-hover:opacity-100'>
+        How to add MDX in Next JS
       </p>
+      <p className='text-xs opacity-60 group-hover:opacity-80'>
+        MDX is a popular library to add Markdown into your React application.
+        Using MDX we can add React components inside markdown.
+      </p>
+      <button className='text-sm opacity-90 group-hover:opacity-100'>
+        See more...
+      </button>
     </div>
   );
 }
--- src/app/(doc)/blogs/page.tsx
+++ src/app/(doc)/blogs/page.tsx
@@ -6,8 +6,8 @@ export default function Page() {
       <h1 className='my-4 text-3xl font-semibold text-center capitalize'>
         Recent blogs by sohanEmon
       </h1>
-      <div className='grid grid-cols-3'>
-        <Card />
+      <div className='grid grid-cols-3 mt-10'>
+        <Card className='col-span-2' />
       </div>
     </main>
   );
