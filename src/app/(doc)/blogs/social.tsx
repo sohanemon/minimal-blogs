f31b00ce1dbdf1ac6d891a6757552adf1b05fa6c
sohanemon <m.sohanemon@gmail.com>
2023-06-12 17:22:09 +0000

    Jun 12, 2023, 11:22 PM

--- src/app/(doc)/blogs/social.tsx
+++ src/app/(doc)/blogs/social.tsx
@@ -1,3 +1,11 @@
+import { Github, Linkedin, Twitter } from 'lucide-react';
+
 export default function Social() {
-  return <></>;
+  return (
+    <>
+      <Github size={20} strokeWidth={1} />
+      <Twitter size={20} strokeWidth={1} />
+      <Linkedin size={20} strokeWidth={1} />
+    </>
+  );
 }
