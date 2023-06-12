198d3a18394d764ba068ac5f26cc69dd672b2e22
sohanemon <m.sohanemon@gmail.com>
2023-06-12 16:53:20 +0000

    Jun 12, 2023, 10:53 PM

--- src/app/(doc)/blogs/card.tsx
+++ src/app/(doc)/blogs/card.tsx
@@ -1,6 +1,6 @@
 export default function Card() {
   return (
-    <div className='@container container max-w-sm border-neutral-400 rounded-xl'>
+    <div className='@container mx-auto px-2 border max-w-sm border-neutral-400 rounded-lg py-2'>
       <p>How to add MDX in Next JS</p>
     </div>
   );
