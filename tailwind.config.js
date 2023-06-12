db8a9ee334cb4736828ccb33f413eac49ce103f4
sohanemon <m.sohanemon@gmail.com>
2023-06-12 16:05:37 +0000

    Jun 12, 2023, 10:05 PM

--- tailwind.config.js
+++ tailwind.config.js
@@ -71,5 +71,5 @@ module.exports = {
       },
     },
   },
-  plugins: [require('tailwindcss-animate'), require('@tailwindcss/typography')],
+  plugins: [require('@tailwindcss/typography'), require('tailwindcss-animate')],
 };
