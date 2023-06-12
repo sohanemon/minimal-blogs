4415e5b6ddeeceb14ea5bf7b585e29e21bb89caf
sohanemon <m.sohanemon@gmail.com>
2023-06-12 21:30:40 +0600

    feat: typography

--- src/app/(doc)/blogs/page.tsx
+++ src/app/(doc)/blogs/page.tsx
@@ -1,8 +1,8 @@
 export default function Page() {
   return (
-    <>
+    <h1 className='prose'>
       Lorem ipsum dolor sit amet consectetur adipisicing elit. Non nostrum eum
       do
-    </>
+    </h1>
   );
 }
